264a0396960309f3f06313bb6c165c1b640039c0
zhangbin <zhangbin@handsem.com>
2015-05-11 17:07:57 +0800

    add ui file

--- src/main/java/com/kakasure/plugin/PagePlugin.java
+++ src/main/java/com/kakasure/plugin/PagePlugin.java
@@ -1,3 +1,4 @@
+
 package com.kakasure.plugin;
 
 import java.lang.reflect.Field;
@@ -34,42 +35,53 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
 import com.kakasure.entity.Page;
 import com.kakasure.util.ReflectHelper;
 import com.kakasure.util.Tools;
+
 /**
  * 
-* 类名称：PagePlugin.java
-* 类描述： 
-* @author fuhang
-* 作者单位： 
-* 联系方式：
-* 创建时间：2014年7月1日
-* @version 1.0
+ * 类名称：PagePlugin.java 类描述：
+ * 
+ * @author fuhang 作者单位： 联系方式： 创建时间：2014年7月1日
+ * @version 1.0
  */
-@Intercepts({@Signature(type=StatementHandler.class,method="prepare",args={Connection.class})})
+@Intercepts({ @Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class }) })
 public class PagePlugin implements Interceptor {
 
-	private static String dialect = "";	//数据库方言
-	private static String pageSqlId = ""; //mapper.xml中需要拦截的ID(正则匹配)
-	
+	private static String	dialect		= "";	// 数据库方言
+	private static String	pageSqlId	= "";	// mapper.xml中需要拦截的ID(正则匹配)
+
 	public Object intercept(Invocation ivk) throws Throwable {
 		// TODO Auto-generated method stub
-		if(ivk.getTarget() instanceof RoutingStatementHandler){
-			RoutingStatementHandler statementHandler = (RoutingStatementHandler)ivk.getTarget();
-			BaseStatementHandler delegate = (BaseStatementHandler) ReflectHelper.getValueByFieldName(statementHandler, "delegate");
-			MappedStatement mappedStatement = (MappedStatement) ReflectHelper.getValueByFieldName(delegate, "mappedStatement");
-			
-			if(mappedStatement.getId().matches(pageSqlId)){ //拦截需要分页的SQL
+		if (ivk.getTarget() instanceof RoutingStatementHandler) {
+			RoutingStatementHandler statementHandler = (RoutingStatementHandler) ivk.getTarget();
+			BaseStatementHandler delegate = (BaseStatementHandler) ReflectHelper.getValueByFieldName(
+					statementHandler, "delegate");
+			MappedStatement mappedStatement = (MappedStatement) ReflectHelper.getValueByFieldName(
+					delegate, "mappedStatement");
+
+			if (mappedStatement.getId().matches(pageSqlId)) { // 拦截需要分页的SQL
 				BoundSql boundSql = delegate.getBoundSql();
-				Object parameterObject = boundSql.getParameterObject();//分页SQL<select>中parameterType属性对应的实体参数，即Mapper接口中执行分页方法的参数,该参数不得为空
-				if(parameterObject==null){
+				Object parameterObject = boundSql.getParameterObject();// 分页SQL<select>中parameterType属性对应的实体参数，即Mapper接口中执行分页方法的参数,该参数不得为空
+				if (parameterObject == null) {
 					throw new NullPointerException("parameterObject尚未实例化！");
-				}else{
+				} else {
 					Connection connection = (Connection) ivk.getArgs()[0];
 					String sql = boundSql.getSql();
-					//String countSql = "select count(0) from (" + sql+ ") as tmp_count"; //记录统计
-					String countSql = "select count(0) from (" + sql+ ")  tmp_count"; //记录统计 == oracle 加 as 报错(SQL command not properly ended)
+					// String countSql = "select count(0) from (" + sql+
+					// ") as tmp_count"; //记录统计
+					String countSql = "select count(0) from (" + sql + ")  tmp_count"; // 记录统计
+																						// ==
+																						// oracle
+																						// 加
+																						// as
+																						// 报错(SQL
+																						// command
+																						// not
+																						// properly
+																						// ended)
 					PreparedStatement countStmt = connection.prepareStatement(countSql);
-					BoundSql countBS = new BoundSql(mappedStatement.getConfiguration(),countSql,boundSql.getParameterMappings(),parameterObject);
-					setParameters(countStmt,mappedStatement,countBS,parameterObject);
+					BoundSql countBS = new BoundSql(mappedStatement.getConfiguration(), countSql,
+							boundSql.getParameterMappings(), parameterObject);
+					setParameters(countStmt, mappedStatement, countBS, parameterObject);
 					ResultSet rs = countStmt.executeQuery();
 					int count = 0;
 					if (rs.next()) {
@@ -77,36 +89,40 @@ public class PagePlugin implements Interceptor {
 					}
 					rs.close();
 					countStmt.close();
-					//System.out.println(count);
+					// System.out.println(count);
 					Page page = null;
-					if(parameterObject instanceof Page){	//参数就是Page实体
-						 page = (Page) parameterObject;
-						 page.setEntityOrField(true);	 //见com.jalan.entity.Page.entityOrField 注释
+					if (parameterObject instanceof Page) { // 参数就是Page实体
+						page = (Page) parameterObject;
+						page.setEntityOrField(true); // 见com.jalan.entity.Page.entityOrField
+														// 注释
 						page.setTotalResult(count);
-					}else{	//参数为某个实体，该实体拥有Page属性
-						Field pageField = ReflectHelper.getFieldByFieldName(parameterObject,"page");
-						if(pageField!=null){
-							page = (Page) ReflectHelper.getValueByFieldName(parameterObject,"page");
-							if(page==null)
+					} else { // 参数为某个实体，该实体拥有Page属性
+						Field pageField = ReflectHelper.getFieldByFieldName(parameterObject, "page");
+						if (pageField != null) {
+							page = (Page) ReflectHelper.getValueByFieldName(parameterObject, "page");
+							if (page == null)
 								page = new Page();
-							page.setEntityOrField(false); //见com.jalan.entity.Page.entityOrField 注释
+							page.setEntityOrField(false); // 见com.jalan.entity.Page.entityOrField
+															// 注释
 							page.setTotalResult(count);
-							ReflectHelper.setValueByFieldName(parameterObject,"page", page); //通过反射，对实体对象设置分页对象
-						}else{
-							throw new NoSuchFieldException(parameterObject.getClass().getName()+"不存在 page 属性！");
+							ReflectHelper.setValueByFieldName(parameterObject, "page", page); // 通过反射，对实体对象设置分页对象
+						} else {
+							throw new NoSuchFieldException(parameterObject.getClass().getName()
+									+ "不存在 page 属性！");
 						}
 					}
-					String pageSql = generatePageSql(sql,page);
-					ReflectHelper.setValueByFieldName(boundSql, "sql", pageSql); //将分页sql语句反射回BoundSql.
+					String pageSql = generatePageSql(sql, page);
+					ReflectHelper.setValueByFieldName(boundSql, "sql", pageSql); // 将分页sql语句反射回BoundSql.
 				}
 			}
 		}
 		return ivk.proceed();
 	}
 
-	
 	/**
-	 * 对SQL参数(?)设值,参考org.apache.ibatis.executor.parameter.DefaultParameterHandler
+	 * 对SQL参数(?)设值,参考org.apache.ibatis.executor.parameter.
+	 * DefaultParameterHandler
+	 * 
 	 * @param ps
 	 * @param mappedStatement
 	 * @param boundSql
@@ -114,13 +130,16 @@ public class PagePlugin implements Interceptor {
 	 * @throws SQLException
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private void setParameters(PreparedStatement ps,MappedStatement mappedStatement,BoundSql boundSql,Object parameterObject) throws SQLException {
-		ErrorContext.instance().activity("setting parameters").object(mappedStatement.getParameterMap().getId());
+	private void setParameters(PreparedStatement ps, MappedStatement mappedStatement,
+			BoundSql boundSql, Object parameterObject) throws SQLException {
+		ErrorContext.instance().activity("setting parameters")
+					.object(mappedStatement.getParameterMap().getId());
 		List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
 		if (parameterMappings != null) {
 			Configuration configuration = mappedStatement.getConfiguration();
 			TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
-			MetaObject metaObject = parameterObject == null ? null: configuration.newMetaObject(parameterObject);
+			MetaObject metaObject = parameterObject == null ? null
+					: configuration.newMetaObject(parameterObject);
 			for (int i = 0; i < parameterMappings.size(); i++) {
 				ParameterMapping parameterMapping = parameterMappings.get(i);
 				if (parameterMapping.getMode() != ParameterMode.OUT) {
@@ -133,51 +152,55 @@ public class PagePlugin implements Interceptor {
 						value = parameterObject;
 					} else if (boundSql.hasAdditionalParameter(propertyName)) {
 						value = boundSql.getAdditionalParameter(propertyName);
-					} else if (propertyName.startsWith(ForEachSqlNode.ITEM_PREFIX)&& boundSql.hasAdditionalParameter(prop.getName())) {
+					} else if (propertyName.startsWith(ForEachSqlNode.ITEM_PREFIX)
+							&& boundSql.hasAdditionalParameter(prop.getName())) {
 						value = boundSql.getAdditionalParameter(prop.getName());
 						if (value != null) {
-							value = configuration.newMetaObject(value).getValue(propertyName.substring(prop.getName().length()));
+							value = configuration.newMetaObject(value).getValue(
+									propertyName.substring(prop.getName().length()));
 						}
 					} else {
 						value = metaObject == null ? null : metaObject.getValue(propertyName);
 					}
 					TypeHandler typeHandler = parameterMapping.getTypeHandler();
 					if (typeHandler == null) {
-						throw new ExecutorException("There was no TypeHandler found for parameter "+ propertyName + " of statement "+ mappedStatement.getId());
+						throw new ExecutorException("There was no TypeHandler found for parameter "
+								+ propertyName + " of statement " + mappedStatement.getId());
 					}
 					typeHandler.setParameter(ps, i + 1, value, parameterMapping.getJdbcType());
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * 根据数据库方言，生成特定的分页sql
+	 * 
 	 * @param sql
 	 * @param page
 	 * @return
 	 */
-	private String generatePageSql(String sql,Page page){
-		if(page!=null && Tools.notEmpty(dialect)){
+	private String generatePageSql(String sql, Page page) {
+		if (page != null && Tools.notEmpty(dialect)) {
 			StringBuffer pageSql = new StringBuffer();
-			if("mysql".equals(dialect)){
+			if ("mysql".equals(dialect)) {
 				pageSql.append(sql);
-				pageSql.append(" limit "+page.getCurrentResult()+","+page.getShowCount());
-			}else if("oracle".equals(dialect)){
+				pageSql.append(" limit " + page.getCurrentResult() + "," + page.getShowCount());
+			} else if ("oracle".equals(dialect)) {
 				pageSql.append("select * from (select tmp_tb.*,ROWNUM row_id from (");
 				pageSql.append(sql);
-				//pageSql.append(") as tmp_tb where ROWNUM<=");
+				// pageSql.append(") as tmp_tb where ROWNUM<=");
 				pageSql.append(") tmp_tb where ROWNUM<=");
-				pageSql.append(page.getCurrentResult()+page.getShowCount());
+				pageSql.append(page.getCurrentResult() + page.getShowCount());
 				pageSql.append(") where row_id>");
 				pageSql.append(page.getCurrentResult());
 			}
 			return pageSql.toString();
-		}else{
+		} else {
 			return sql;
 		}
 	}
-	
+
 	public Object plugin(Object arg0) {
 		return Plugin.wrap(arg0, this);
 	}
@@ -200,5 +223,5 @@ public class PagePlugin implements Interceptor {
 			}
 		}
 	}
-	
+
 }
