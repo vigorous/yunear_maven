04a6b6236a8f6c5fe066c6cc204a00a52f5d7fd3
zhangbin <zhangbin@kakasure.com>
2015-05-14 10:53:39 +0800

    user add blance

--- src/main/java/com/kakasure/entity/system/User.java
+++ src/main/java/com/kakasure/entity/system/User.java
@@ -1,6 +1,8 @@
+
 package com.kakasure.entity.system;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import com.kakasure.entity.Page;
 
@@ -12,19 +14,20 @@ import com.kakasure.entity.Page;
  * @version 1.0
  */
 public class User implements Serializable {
-	private static final long serialVersionUID = -3271684710587883956L;
-	private String USER_ID; // 用户id
-	private String USERNAME; // 用户名
-	private String PASSWORD; // 密码
-	private String NAME; // 姓名
-	private String RIGHTS; // 权限
-	private String ROLE_ID; // 角色id
-	private String LAST_LOGIN; // 最后登录时间
-	private String IP; // 用户登录ip地址
-	private String STATUS; // 状态
-	private Role role; // 角色对象
-	private Page page; // 分页对象
-	private String SKIN; // 皮肤
+	private static final long	serialVersionUID	= -3271684710587883956L;
+	private String				USER_ID;										// 用户id
+	private String				USERNAME;										// 用户名
+	private String				PASSWORD;										// 密码
+	private String				NAME;											// 姓名
+	private String				RIGHTS;										// 权限
+	private String				ROLE_ID;										// 角色id
+	private String				LAST_LOGIN;									// 最后登录时间
+	private String				IP;											// 用户登录ip地址
+	private String				STATUS;										// 状态
+	private Role				role;											// 角色对象
+	private Page				page;											// 分页对象
+	private String				SKIN;											// 皮肤
+	private BigDecimal			BALANCE;										// 余额
 
 	public String getSKIN() {
 		return SKIN;
@@ -122,6 +125,14 @@ public class User implements Serializable {
 
 	public void setPage(Page page) {
 		this.page = page;
+	}
+
+	public BigDecimal getBALANCE() {
+		return BALANCE;
+	}
+
+	public void setBALANCE(BigDecimal bALANCE) {
+		BALANCE = bALANCE;
 	}
 
 }
